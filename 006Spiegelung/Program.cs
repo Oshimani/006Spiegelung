8a67aafb66faa260305cd39fc920a16b8a40add8
Jannick Jungbluth <jannick2.0@googlemail.com>
2017-03-30 19:41:48 +0200

    Init

---
+++ 006Spiegelung/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006Spiegelung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String satz = Console.ReadLine();
+            char [] ergebnis = satz.ToArray<char>(); // so erhält man die selbe Länge
+            Console.WriteLine(ergebnis);
+            int zaehler = 0; // stellenindex
+            while (zaehler < satz.Length)
+            {
+                ergebnis[ergebnis.Length - zaehler -1] = satz[zaehler];
+                zaehler = zaehler + 1;
+            }
+            Console.WriteLine(ergebnis);
+        }
+    }
+}
